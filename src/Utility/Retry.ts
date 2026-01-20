64ce8667fb2bf3a7353d91e6b8d71a15254ad8d0
adi-rthd <80950864+adi-rthd@users.noreply.github.com>
2026-01-20 10:51:52 +0530

    Update Retry.ts

--- src/Utility/Retry.ts
+++ src/Utility/Retry.ts
@@ -9,14 +9,18 @@ import { RetryOptions } from "../Interfaces/RetryOptions";
 export async function withRetry<T>(fn: () => Promise<T>, options: RetryOptions): Promise<T> {
     const {
         attempts,
-        backOffMs = 300, //5 mins
-        retryOn = ["NETWORK", "TIMEOUT", "RATE_LIMIT", "SERVER"]
+        backOffMs = 30000, //5 mins
+        retryOn = ["NETWORK", "TIMEOUT", "RATE_LIMIT", "SERVER", "UNKNOWN"]
     } = options
     let lastError: any;
 
     for (let i = 0; i < attempts; i++) {
         try {
-            return await fn();
+            let { data, error } = await fn()
+            if (!!error) {
+                throw error
+            }
+            return { data, error };        
         } catch (err) {
             let classified = classifyError(err)
             lastError = classified
